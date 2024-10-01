e4a8cff14b2e7c94003739d14af5ae5f269ac774
NACHORTE <ignaciortegape@gmail.com>
2024-10-01 19:15:31 +0200

    [main]<2024-10-01-19:15> admin

--- Makefile
+++ Makefile
@@ -8,12 +8,27 @@ VOLUMES := mariadb wordpress
 
 VOLUME = $(addprefix $(VOL_DIR)/,$(VOLUMES))
 
-RM = rm -rf 
+RM = rm -rf
+
+# Leer el archivo .env
+include srcs/.env
+
+# Lista de nombres de administrador prohibidos
+FORBIDDEN_USERS := admin Admin administrator Administrator
+
+# Función para verificar si ADMIN_USER está en FORBIDDEN_USERS
+define check_admin_user
+	$(foreach forbidden,$(FORBIDDEN_USERS),\
+		$(if $(findstring $(forbidden),$(ADMIN_USER)),\
+			$(error "ADMIN_USER contiene un valor no permitido: $(ADMIN_USER)")))
+endef
 
 run: $(VOLUME)
+	$(call check_admin_user)
 	sudo docker compose -f $(YML_PATH) up --build --remove-orphans
 
 dt: $(VOLUME)
+	$(call check_admin_user)
 	sudo docker compose -f $(YML_PATH) up --build -d --remove-orphans
 
 down:
@@ -21,7 +36,7 @@ down:
 
 re:
 	$(RM) $(VOL_DIR)
-	make 
+	make
 
 clean:
 	sudo docker compose -f $(YML_PATH) down -v --remove-orphans --rmi all
--- srcs/requirements/mariadb/tools/MariaSetUp.sh
+++ srcs/requirements/mariadb/tools/MariaSetUp.sh
@@ -11,7 +11,7 @@ function add_query_line {
 chown -R mysql: /var/lib/mysql
 chmod 777 /var/lib/mysql
 
-mysql_install_db >/dev/null 2>&1
+mysql_install_db --skip-test-db >/dev/null 2>&1
 
 if [ ! -d "/var/lib/mysql/$MYSQL_DATABASE" ]; then
   rm -f "$MYSQL_INIT_FILE"
@@ -20,10 +20,7 @@ if [ ! -d "/var/lib/mysql/$MYSQL_DATABASE" ]; then
   add_query_line "CREATE USER $MYSQL_USER@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';"
   add_query_line "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO $MYSQL_USER@'%' WITH GRANT OPTION;"
   add_query_line "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO $MYSQL_USER@'localhost' WITH GRANT OPTION;"
-  add_query_line "FLUSH PRIVILEGES;"
-  add_query_line "DROP USER 'root'@'localhost';"
-  add_query_line "CREATE USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
-  add_query_line "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;"
+  add_query_line "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
   add_query_line "FLUSH PRIVILEGES;"
   print_log "Starting MariDB server..."
   mysqld_safe --init-file=$MYSQL_INIT_FILE >/dev/null 2>&1
